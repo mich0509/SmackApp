2a200ba75b6a5e7285913699c079de4c70cbb420
Michelle Sajorda <michellesajorda@Michelles-MacBook-Pro.local>
2020-08-28 04:51:15 +0800

    create user activity UI

--- app/src/main/java/com/example/smack/LoginActivity.kt
+++ app/src/main/java/com/example/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.smack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -18,7 +19,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginCreateBtnClicked(view: View) {
-
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
 
     }
 }
