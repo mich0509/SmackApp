d69a85bbaa4aa888d5ae1f262e0745eb3eb0dc2c
Michelle Sajorda <michellesajorda@Michelles-MacBook-Pro.local>
2020-08-28 04:20:18 +0800

    Login Activity UI

---
+++ app/src/main/java/com/example/smack/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.example.smack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginLoginBtnClicked(view: WebView) {
+
+
+    }
+
+    fun loginCreateBtnClicked(view: View) {
+
+
+    }
+}
